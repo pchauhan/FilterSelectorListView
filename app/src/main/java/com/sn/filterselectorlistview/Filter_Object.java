e3b59eedc1188e7b2cbc4258e0ab476e59f97b4a
Urvi <urvi@lionasys.in>
2017-12-20 18:08:01 +0530

    initial commit

---
+++ app/src/main/java/com/sn/filterselectorlistview/Filter_Object.java
@@ -0,0 +1,9 @@
+package com.sn.filterselectorlistview;
+
+/**
+ * Created by parag.chauhan on 7/28/2015.
+ */
+public class Filter_Object {
+    public String mName ;
+    public boolean mIsSelected ;
+}
---
+++ app/src/main/java/com/sn/filterselectorlistview/MainActivity.java
@@ -0,0 +1,219 @@
+package com.sn.filterselectorlistview;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
+    private ArrayList<Filter_Object> mArrFilter;
+    private ScrollView mScrollViewFilter;
+    private Filter_Adapter mFilter_Adapter ;
+    private FlowLayout mFlowLayoutFilter ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            getWindow().setStatusBarColor(getResources().getColor(R.color.themecolor));
+        }
+
+        mArrFilter = new ArrayList<>();
+
+        String[] strArr = getResources().getStringArray(R.array.country);
+
+        int lengthOfstrArr = strArr.length;
+
+        for (int i = 0; i < lengthOfstrArr; i++) {
+            Filter_Object filter_object = new Filter_Object();
+            filter_object.mName = strArr[i];
+            filter_object.mIsSelected = false;
+            mArrFilter.add(filter_object);
+        }
+
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setTitle(getString(R.string.app_name));
+
+        mListView = (ListView) findViewById(R.id.listViewFilter);
+        mScrollViewFilter = (ScrollView)findViewById(R.id.scrollViewFilter);
+        mFlowLayoutFilter = (FlowLayout)findViewById(R.id.flowLayout);
+
+        mFilter_Adapter = new Filter_Adapter(mArrFilter);
+        mListView.setAdapter(mFilter_Adapter);
+    }
+    public void addFilterTag() {
+        final ArrayList<Filter_Object> arrFilterSelected = new ArrayList<>();
+
+        mFlowLayoutFilter.removeAllViews();
+
+        int length = mArrFilter.size();
+        boolean isSelected = false;
+        for (int i = 0; i < length; i++) {
+            Filter_Object fil = mArrFilter.get(i);
+            if (fil.mIsSelected) {
+                isSelected = true;
+                arrFilterSelected.add(fil);
+            }
+        }
+        if (isSelected) {
+            mScrollViewFilter.setVisibility(View.VISIBLE);
+        } else {
+            mScrollViewFilter.setVisibility(View.GONE);
+        }
+        int size = arrFilterSelected.size();
+        LayoutInflater layoutInflater = (LayoutInflater)
+                this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        for (int i = 0; i < size; i++) {
+            View view = layoutInflater.inflate(R.layout.filter_tag_edit, null);
+
+            TextView tv = (TextView) view.findViewById(R.id.tvTag);
+            LinearLayout linClose = (LinearLayout) view.findViewById(R.id.linClose);
+            final Filter_Object filter_object = arrFilterSelected.get(i);
+            linClose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //showToast(filter_object.name);
+
+
+                    int innerSize = mArrFilter.size();
+                    for (int j = 0; j < innerSize; j++) {
+                        Filter_Object mFilter_Object = mArrFilter.get(j);
+                        if (mFilter_Object.mName.equalsIgnoreCase(filter_object.mName)) {
+                            mFilter_Object.mIsSelected = false;
+
+                        }
+                    }
+                    addFilterTag();
+                    mFilter_Adapter.updateListView(mArrFilter);
+                }
+            });
+
+
+            tv.setText(filter_object.mName);
+            int color = getResources().getColor(R.color.themecolor);
+
+            View newView = view;
+            newView.setBackgroundColor(color);
+
+            FlowLayout.LayoutParams params = new FlowLayout.LayoutParams(FlowLayout.LayoutParams.WRAP_CONTENT, FlowLayout.LayoutParams.WRAP_CONTENT);
+            params.rightMargin = 10;
+            params.topMargin = 5;
+            params.leftMargin = 10;
+            params.bottomMargin = 5;
+
+            newView.setLayoutParams(params);
+
+            mFlowLayoutFilter.addView(newView);
+        }
+    }
+
+    public class Filter_Adapter extends BaseAdapter {
+        ArrayList<Filter_Object> arrMenu;
+
+        public Filter_Adapter(ArrayList<Filter_Object> arrOptions) {
+            this.arrMenu = arrOptions;
+        }
+
+        public void updateListView(ArrayList<Filter_Object> mArray) {
+            this.arrMenu = mArray;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return this.arrMenu.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.filter_list_item, null);
+                viewHolder = new ViewHolder();
+                viewHolder.mTtvName = (TextView) convertView.findViewById(R.id.tvName);
+                viewHolder.mTvSelected = (TextView) convertView.findViewById(R.id.tvSelected);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            final Filter_Object mService_Object = arrMenu.get(position);
+            viewHolder.mTtvName.setText(mService_Object.mName);
+
+            if (mService_Object.mIsSelected) {
+                viewHolder.mTvSelected.setVisibility(View.VISIBLE);
+            } else {
+                viewHolder.mTvSelected.setVisibility(View.INVISIBLE);
+            }
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    mService_Object.mIsSelected = !mService_Object.mIsSelected;
+                    mScrollViewFilter.setVisibility(View.VISIBLE);
+
+                    addFilterTag();
+                    notifyDataSetChanged();
+                }
+            });
+            return convertView;
+        }
+
+        public class ViewHolder {
+            TextView mTtvName, mTvSelected;
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
